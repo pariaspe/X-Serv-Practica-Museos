4fd0b28cc57211573a07330576ce386e2945d644
pariaspe <pedroariasperez96@gamil.com>
2018-05-09 00:46:19 +0200

    Views con una funcion basica para cada url creado

--- myproject/museos/views.py
+++ myproject/museos/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def barra(request):
+    return HttpResponse('Barra')
+
+def museos(request):
+    return HttpResponse('Museos')
+
+def museo_id(request, mid):
+    return HttpResponse('Museo ' + mid)
+
+def usuario(request, name):
+    return HttpResponse('Usuario ' + name)
+
+def usuario_xml(request, name):
+    return HttpResponse('Xml de ' + name)
+
+def about(request):
+    return HttpResponse('About')
