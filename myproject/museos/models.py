1c4a390311bdc637c481e21e73c4d721f870c1f7
pariaspe <pedroariasperez96@gamil.com>
2018-05-11 11:43:21 +0200

    Primera version del modelo desarrollada

--- myproject/museos/models.py
+++ myproject/museos/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    nombre = models.CharField()
+    contraseña = models.CharField()
+    pagina = models.CharField()
+    usuario_museo = models.ManyToManiField(Museo)
+    
+    def __str__(self):
+        return self.nombre
+
+class Museo(models.Model):
+    nombre = models.CharField()
+
+    def __str__(self):
+        return self.nombre
+
+class Comentario(models.Model):
+    usuario = models.ForeignKey(Usuario)
+    museo = models.ForeignKey(Museo)
+    comentario = models.TextField()
+    fecha = models.DateTimeField()
+
+    def __str__(self):
+        return self.nomre + " " + self.fecha
