f8eb51bf8085e64759a04397016d8137e5b8bab3
pariaspe <pedroariasperez96@gamil.com>
2018-05-11 11:55:23 +0200

    Modelos registrados

--- myproject/museos/admin.py
+++ myproject/museos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Museo, Usuario, Comentario
 
 # Register your models here.
+admin.site.register(Museo)
+admin.site.register(Usuario)
+admin.site.register(Comentario)
